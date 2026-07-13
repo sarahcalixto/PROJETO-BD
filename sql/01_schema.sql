f46215fb27343375eb8f61b7dc8bdbc1beef0596
RuanCampello <ruancampello999@gmail.com>
2026-07-13 18:10:47 -0300

    feat(schema): add three main tables ddl

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -1,2 +1,22 @@
--- Arquivo reservado para o integrante responsável.
--- Não implementar nesta branch.
+create table pessoa (
+  id serial primary key,
+  nome varchar(255) not null,
+  cpf varchar(11) unique not null,
+  data_nascimento date,
+  is_flamengo boolean default true,
+  telefone varchar(20),
+);
+
+create table paciente (
+  id int primary key references pessoa(id) on delete cascade,
+  num_convenio varchar(50),
+  alergias text, -- TODO: maybe we can model this better
+  grupo_sanguineo varchar(3),
+);
+
+create table profissional (
+  id int primary key references pessoa(id) on delete cascade,
+  crm varchar(20) unique,
+  data_admissao date,
+  especialidade text,
+);
