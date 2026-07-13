2953f23a486adccc8014e30400ff65bb0492469c
RuanCampello <ruancampello999@gmail.com>
2026-07-13 18:32:19 -0300

    feat(schema): add first wacky implementation

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -58,3 +58,34 @@ create table atendimento (
   id_unidade int references unidade(id) on delete cascade,
 );
 
+create type risco as enum ('baixo', 'medio', 'alto');
+
+create table procedimento (
+  id serial primary key,
+  codigo int unique not null,
+  nome varchar(255) not null,
+  tempo_medio_minutos int check (tempo_medio_minutos >= 0),
+  nivel_risco risco not null,
+);
+
+create table procedimento_realizado (
+  id_atendimento int references atendimento(id),
+  id_procedimento int references procedimento(id),
+  quantidade int check (quantidade >= 0),
+  tempo_real_minutos int check (tempo_medio_minutos >= 0),
+  observacao text,
+  faturado boolean default false,
+
+  primary key (id_atendimento, id_procedimento)
+);
+
+create type turno as enum ('manha', 'tarde', 'noite');
+
+create table escala (
+  id serial primary key,
+  id_unidade int references unidade(id),
+  data_plantao date not null,
+  turno turno not null,
+  id_atuacao_residente int references atuacao_residente(id),
+  id_atuacao_preceptor int references atuacao_preceptor(id),
+)
